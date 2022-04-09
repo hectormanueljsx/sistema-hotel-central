087dfa65dd755c236190a445cfc145727627f692
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-04-09 15:28:15 -0500

    Add install prettier and config

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  printWidth: 120,
+};
