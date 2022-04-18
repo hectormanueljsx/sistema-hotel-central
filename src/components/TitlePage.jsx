18daf910bc70a3b9e48f57db3e4bfaac503ea496
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-04-18 16:22:51 -0500

    Update TitlePage.jsx

--- src/components/TitlePage.jsx
+++ src/components/TitlePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Typography } from '@mui/material';
 
+import { stylesTitlePage } from './stylesGlobals';
+
 const TitlePage = ({ titlePage }) => {
   return (
-    <Typography component='h2' sx={{ fontSize: 24, fontWeight: 700, textAlign: 'center', marginBottom: 3 }}>
+    <Typography component='h2' sx={stylesTitlePage}>
       {titlePage}
     </Typography>
   );
