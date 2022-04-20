0b1b4c42adb29d1464d5cd63d1bdb68f964c5fd7
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-04-20 16:57:38 -0500

    Create TableViewCategoriaEgresos.jsx

---
+++ src/components/Caja/CategoriaEgresos/TableViewCategoriaEgresos.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  Box,
+  Container,
+  CssBaseline,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+
+import TitlePage from '../../TitlePage';
+import { stylesContainerSection, stylesTableCell } from '../stylesCaja';
+
+const columns = [
+  { id: 'desc_categoria', label: 'Descripción Categoría', width: 400 },
+  { id: 'desc_subcategoria', label: 'Descripción Subcategoría', width: 600 },
+];
+
+const TableViewCategoriaEgresos = () => {
+  return (
+    <Container component='section' sx={[stylesContainerSection, { width: 1000 }]}>
+      <CssBaseline />
+      <TitlePage titlePage='Lista de Categorías Registradas' />
+      <Box component='div'>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                {columns.map((column, index) => (
+                  <TableCell key={index} sx={[stylesTableCell, { width: column.width }]}>
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell sx={stylesTableCell}>CONSUMIBLES</TableCell>
+                <TableCell sx={stylesTableCell}>REFRESCOS</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Box>
+    </Container>
+  );
+};
+
+export default TableViewCategoriaEgresos;
