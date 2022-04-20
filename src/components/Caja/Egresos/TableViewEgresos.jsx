4cf28dcbbd023243843fdcb78c4a4c2da8f5d637
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-04-20 13:30:08 -0500

    Update TableViewEgresos.jsx

--- src/components/Caja/Egresos/TableViewEgresos.jsx
+++ src/components/Caja/Egresos/TableViewEgresos.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Box,
   Container,
@@ -8,43 +8,22 @@ import {
   TableCell,
   TableContainer,
   TableHead,
-  TablePagination,
   TableRow,
 } from '@mui/material';
+
 import TitlePage from '../../TitlePage';
-import { stylesContainerSection } from '../stylesCaja';
-import useGetSpecific from '../../../hooks/useGetSpecific';
+import { stylesContainerSection, stylesTableCell } from '../stylesCaja';
 
 const columns = [
-  { id: 'num_gasto', label: 'N° Gasto', width: 252 },
-  { id: 'fecha', label: 'Fecha', width: 200 },
-  { id: 'concepto', label: 'Concepto', width: 300 },
-  { id: 'categoria', label: 'Categoria', width: 300 },
-  { id: 'importe', label: 'Importe', width: 300 },
-  { id: 'detalle', label: 'Detalle', width: 300 },
+  { id: 'num_gasto', label: 'N° Gasto', width: 120 },
+  { id: 'fecha', label: 'Fecha', width: 280 },
+  { id: 'concepto', label: 'Concepto', width: 200 },
+  { id: 'categoria', label: 'Categoria', width: 200 },
+  { id: 'importe', label: 'Importe', width: 100 },
+  { id: 'detalle', label: 'Detalle', width: 100 },
 ];
 
-const TableViewGasto = () => {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-
-  const identifier = 'test@email.com';
-  const password = 'Test123';
-  const endpoint = 'egresos';
-  const atribute = 'facturado';
-  const parameter = 1;
-  const { list, loading, error } = useGetSpecific(identifier, password, endpoint, atribute, parameter);
-  console.log(list);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
+const TableViewEgresos = () => {
   return (
     <Container component='section' sx={[stylesContainerSection, { width: 1000 }]}>
       <CssBaseline />
@@ -55,39 +34,27 @@ const TableViewGasto = () => {
             <TableHead>
               <TableRow>
                 {columns.map((column, index) => (
-                  <TableCell key={index} sx={{ width: column.width }}>
+                  <TableCell key={index} sx={[stylesTableCell, { width: column.width }]}>
                     {column.label}
                   </TableCell>
                 ))}
               </TableRow>
             </TableHead>
             <TableBody>
-              {loading && console.log('Cargando')}
-              {error && console.log('Error')}
-              {/* {list &&
-                list.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row, index) => (
-                  <TableRow key={index}>
-                    {
-                      const { nombre, rol, ultimoIngreso } = row
-                      // <TableCell key={row.id}>{row}</TableCell>
-                    }
-                  </TableRow>
-                ))} */}
+              <TableRow>
+                <TableCell sx={stylesTableCell}>16913</TableCell>
+                <TableCell sx={stylesTableCell}>2022-02-23 12:08:35</TableCell>
+                <TableCell sx={stylesTableCell}>IMPRESIONES</TableCell>
+                <TableCell sx={stylesTableCell}>PAPELERIA</TableCell>
+                <TableCell sx={stylesTableCell}>1.00</TableCell>
+                <TableCell sx={stylesTableCell}>Ver</TableCell>
+              </TableRow>
             </TableBody>
           </Table>
         </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[]}
-          component='div'
-          count={list.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
       </Box>
     </Container>
   );
 };
 
-export default TableViewGasto;
+export default TableViewEgresos;
