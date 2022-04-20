18ece58cfe1b254d856e94587aa601eb39e67660
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-04-20 16:44:14 -0500

    Create FormCreateCategoriaEgresos.jsx

---
+++ src/components/Caja/CategoriaEgresos/FormCreateCategoriaEgresos.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Box, Button, Container, CssBaseline, FormControl, Select, TextField } from '@mui/material';
+import SaveIcon from '@mui/icons-material/Save';
+
+import TitlePage from '../../TitlePage';
+import TitleInput from '../../TitleInput';
+import { stylesContainerBox, stylesContainerInput, stylesContainerSection } from '../stylesCaja';
+
+const FormCreateCategoriaEgresos = () => {
+  return (
+    <>
+      <Container component='section' sx={[stylesContainerSection, { width: 400, height: 250.25 }]}>
+        <CssBaseline />
+        <TitlePage titlePage='Registro de Categoría' />
+        <Box component='form' sx={stylesContainerBox}>
+          <Box component='div' sx={stylesContainerInput}>
+            <TitleInput titleInput='Nombre de la categoría' />
+            <TextField
+              variant='outlined'
+              type='text'
+              margin='none'
+              size='small'
+              placeholder='Salarios'
+              required
+              fullWidth
+              autoFocus
+            />
+          </Box>
+          <Button variant='contained' size='large' startIcon={<SaveIcon />} sx={{ marginTop: 2 }}>
+            Registrar Categoría
+          </Button>
+        </Box>
+      </Container>
+      <Container component='section' sx={[stylesContainerSection, { width: 400, height: 334.25 }]}>
+        <CssBaseline />
+        <TitlePage titlePage='Registro de Subcategoría' />
+        <Box component='form' sx={stylesContainerBox}>
+          <Box component='div' sx={stylesContainerInput}>
+            <TitleInput titleInput='Seleccione categoría' />
+            <FormControl fullWidth>
+              <Select size='small'></Select>
+            </FormControl>
+          </Box>
+          <Box component='div' sx={stylesContainerInput}>
+            <TitleInput titleInput='Nombre de la subcategoria' />
+            <TextField
+              variant='outlined'
+              type='text'
+              margin='none'
+              size='small'
+              placeholder='Recepcionista'
+              required
+              fullWidth
+              autoFocus
+            />
+          </Box>
+          <Button variant='contained' size='large' startIcon={<SaveIcon />} sx={{ marginTop: 2 }}>
+            Registrar Subcategoría
+          </Button>
+        </Box>
+      </Container>
+    </>
+  );
+};
+
+export default FormCreateCategoriaEgresos;
