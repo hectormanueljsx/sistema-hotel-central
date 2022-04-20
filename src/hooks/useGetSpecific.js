492d7813fa22d83ef65c370d08df28b71709b9db
alesnchz8 <alesnchz4@gmail.com>
2022-04-19 20:02:16 -0500

    Create useGetSpecific.js

---
+++ src/hooks/useGetSpecific.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import apiConfig from '../api/apiConfig';
+import getTokenUser from '../services/getTokenUser';
+
+const useGetGeneralTable = (identifier, password, endpoint, atribute, parameter) => {
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const getData = async () => {
+    try {
+      setLoading(true);
+
+      const userToken = await getTokenUser(identifier, password);
+      const { data } = await apiConfig.get(`${endpoint}?${atribute}=${parameter}`, {
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+
+      setList(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { list, loading, error };
+};
+
+export default useGetGeneralTable;
