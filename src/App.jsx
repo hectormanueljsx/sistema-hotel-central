77f98fb4e4148bdee0833463f98ecec266461a2c
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-04-22 17:36:14 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#f5f6fa',
+    },
+  },
+});
 
 const App = () => {
-  return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
-  );
+  const identifier = 'test@email.com';
+  const password = 'Test123';
+
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 };
 
 export default App;
