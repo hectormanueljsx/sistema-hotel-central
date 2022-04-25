19b1e6f9ea0cb7079dcbb633396c3055ce52e9aa
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-04-25 13:39:00 -0500

    Create FormAnticipo.jsx

---
+++ src/components/Reportes/Anticipo/FormAnticipo.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Box, Button, Container, CssBaseline, FormControl, Select, TextField } from '@mui/material';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import BackspaceRoundedIcon from '@mui/icons-material/BackspaceRounded';
+
+import TitlePage from '@/components/TitlePage';
+import TitleInput from '@/components/TitleInput';
+import { stylesContainerBox, stylesContainerInput, stylesContainerSection } from '@/components/Reportes/stylesReportes';
+
+const FormAnticipo = () => {
+  return (
+    <Container component='section' sx={[stylesContainerSection, { width: 400, height: 418.25 }]}>
+      <CssBaseline />
+      <TitlePage titlePage='Anticipos por Periodo' />
+      <Box component='form' sx={stylesContainerBox}>
+        <Box component='div' sx={stylesContainerInput}>
+          <TitleInput titleInput='De fecha:' />
+          <TextField variant='outlined' type='date' margin='none' size='small' required fullWidth autoFocus />
+        </Box>
+        <Box component='div' sx={stylesContainerInput}>
+          <TitleInput titleInput='A fecha:' />
+          <TextField variant='outlined' type='date' margin='none' size='small' required fullWidth />
+        </Box>
+        <Box component='div' sx={stylesContainerInput}>
+          <TitleInput titleInput='Forma de pago' />
+          <FormControl fullWidth>
+            <Select size='small'></Select>
+          </FormControl>
+        </Box>
+        <Box component='div' sx={{ marginTop: 2 }}>
+          <Button variant='contained' size='large' startIcon={<BackspaceRoundedIcon />} sx={{ marginRight: 2 }}>
+            Limpiar
+          </Button>
+          <Button variant='contained' size='large' startIcon={<SearchRoundedIcon />}>
+            Buscar
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
+
+export default FormAnticipo;
