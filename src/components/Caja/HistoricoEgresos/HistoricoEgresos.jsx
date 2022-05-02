287932b3065834342df2c8e504f7d48c21bb21e7
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-02 18:09:20 -0500

    Update HistoricoEgresos.jsx

--- src/components/Caja/HistoricoEgresos/HistoricoEgresos.jsx
+++ src/components/Caja/HistoricoEgresos/HistoricoEgresos.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Box, Container, CssBaseline } from '@mui/material';
 
-import TableViewHistoricoEgresos from './TableViewHistoricoEgresos';
+import FormSearchHistoricoEgresos from '@/components/Caja/HistoricoEgresos/FormSearchHistoricoEgresos';
+import TableViewHistoricoEgresos from '@/components/Caja/HistoricoEgresos/TableViewHistoricoEgresos';
 
 const HistoricoEgresos = () => {
   return (
     <Container maxWidth='xl'>
       <CssBaseline />
       <Box sx={{ display: 'flex' }}>
+        <FormSearchHistoricoEgresos />
         <TableViewHistoricoEgresos />
       </Box>
     </Container>
