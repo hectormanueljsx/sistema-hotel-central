cfbc6e2cd9f109087456c063de4c58633e478b68
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-06 19:42:00 -0500

    Update TableViewCategoriaEgresos.jsx

--- src/components/Caja/CategoriaEgresos/TableViewCategoriaEgresos.jsx
+++ src/components/Caja/CategoriaEgresos/TableViewCategoriaEgresos.jsx
@@ -1,49 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
   Box,
   Container,
   CssBaseline,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
+  ListItemIcon,
+  Typography,
 } from '@mui/material';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
 
 import TitlePage from '@/components/TitlePage';
-import { stylesContainerSection, stylesTableCell } from '@/components/Caja/stylesCaja';
-
-const columns = [
-  { id: 'desc_categoria', label: 'Descripción Categoría', width: 352 },
-  { id: 'desc_subcategoria', label: 'Descripción Subcategoría', width: 600 },
-];
+import Loader from '@/components/Loader/Loader';
+import useGetGeneralTable from '@/hooks/useGetGeneralTable';
+import { generalEndpoints } from '@/utilities/endpoints';
+import { stylesContainerSection } from '@/components/Caja/stylesCaja';
 
 const TableViewCategoriaEgresos = () => {
+  const [expanded, setExpanded] = useState(false);
+
+  const identifier = 'test@email.com';
+  const password = 'Test123';
+  const endpoint = generalEndpoints.categoria;
+
+  const { list, loading, error } = useGetGeneralTable(identifier, password, endpoint);
+
+  const handleChange = panel => (event, isExpanded) => setExpanded(isExpanded ? panel : false);
+
   return (
     <Container component='section' sx={[stylesContainerSection, { width: 1000 }]}>
       <CssBaseline />
       <TitlePage titlePage='Lista de Categorías Registradas' />
       <Box component='div'>
-        <TableContainer>
-          <Table>
-            <TableHead>
-              <TableRow>
-                {columns.map((column, index) => (
-                  <TableCell key={index} sx={[stylesTableCell, { width: column.width }]}>
-                    {column.label}
-                  </TableCell>
-                ))}
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              <TableRow>
-                <TableCell sx={stylesTableCell}>CONSUMIBLES</TableCell>
-                <TableCell sx={stylesTableCell}>REFRESCOS</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
+        {loading && <Loader />}
+        {list.map((item, index) => {
+          const { categoria, subcategorias } = item;
+
+          return (
+            <Accordion key={index} expanded={expanded === `panel${index}`} onChange={handleChange(`panel${index}`)}>
+              <AccordionSummary key={index} expandIcon={<ExpandMore />}>
+                <Typography key={index} sx={{ fontWeight: '700' }}>
+                  {categoria}
+                </Typography>
+              </AccordionSummary>
+              {subcategorias.map((value, idx) => {
+                return (
+                  <AccordionDetails
+                    key={idx}
+                    sx={{ display: 'flex', alignItems: 'center', padding: 0, marginBottom: 2 }}
+                  >
+                    <ListItemIcon>
+                      <ArrowRightIcon sx={{ marginLeft: 5 }} />
+                    </ListItemIcon>
+                    <Typography key={idx} sx={{ fontWeight: '300' }}>
+                      {value.descripcion}
+                    </Typography>
+                  </AccordionDetails>
+                );
+              })}
+            </Accordion>
+          );
+        })}
       </Box>
     </Container>
   );
