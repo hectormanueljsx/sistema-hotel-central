d312e7070f36d6572ae2bcc28c81e4eb16db13de
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-09 13:56:17 -0500

    Update putUsers.js

--- src/services/putUsers.js
+++ src/services/putUsers.js
@@ -1,16 +1,16 @@
 import apiConfig from '@/api/apiConfig';
 import getTokenUser from '@/services/getTokenUser';
 
-const putUsers = async (identifier, password, dataUser, dataRole) => {
+const putUsers = async (identifier, password, endpoint, idUser, dataUser, dataRole) => {
   try {
     const userToken = await getTokenUser(identifier, password);
-    const { data } = await apiConfig.put(`${endpoint}/${id_user}`, dataUser, {
+    const { data } = await apiConfig.put(`${endpoint}/${idUser}`, dataUser, {
       headers: {
         Authorization: `Bearer ${userToken}`,
       },
     });
 
-    const { role } = await apiConfig.put(`${endpoint}/${id_user}`, dataRole, {
+    const { role } = await apiConfig.put(`${endpoint}/${idUser}`, dataRole, {
       headers: {
         Authorization: `Bearer ${userToken}`,
       },
