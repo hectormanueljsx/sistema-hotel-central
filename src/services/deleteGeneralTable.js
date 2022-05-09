ce14f864882a1cb4a9ca3779795f94c11fec5e5a
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-09 10:48:58 -0500

    Create deleteGeneralTable.js

---
+++ src/services/deleteGeneralTable.js
@@ -0,0 +1,19 @@
+import apiConfig from '@/api/apiConfig';
+import getTokenUser from '@/services/getTokenUser';
+
+const deleteGeneralTable = async (identifier, password, endpoint, id) => {
+  try {
+    const userToken = await getTokenUser(identifier, password);
+    const { data } = await apiConfig.delete(`${endpoint}/${id}`, {
+      headers: {
+        Authorization: `Bearer ${userToken}`,
+      },
+    });
+
+    return data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export { deleteGeneralTable };
