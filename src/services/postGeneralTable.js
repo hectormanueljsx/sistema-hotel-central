7a802a4655044e9398117e5aee287ede5bca5f60
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-09 10:47:32 -0500

    Update postGeneralTable.js

--- src/services/postGeneralTable.js
+++ src/services/postGeneralTable.js
@@ -4,7 +4,7 @@ import getTokenUser from '@/services/getTokenUser';
 const postGeneralTable = async (identifier, password, endpoint, generalData) => {
   try {
     const userToken = await getTokenUser(identifier, password);
-    const data = await apiConfig.post(endpoint, generalData, {
+    const { data } = await apiConfig.post(endpoint, generalData, {
       headers: {
         Authorization: `Bearer ${userToken}`,
       },
