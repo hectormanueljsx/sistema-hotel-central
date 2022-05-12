f2d3070ee3ea6c7e18381b9b4ce36ea5af322717
alesnchz8 <alesnchz4@gmail.com>
2022-05-11 19:08:01 -0500

    Update FormCreateUsuario.jsx

--- src/components/Habitaciones/Usuario/FormCreateUsuario.jsx
+++ src/components/Habitaciones/Usuario/FormCreateUsuario.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { Box, Button, Container, CssBaseline, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { Box, Button, Container, CssBaseline, TextField, Checkbox, FormControlLabel } from '@mui/material';
 import SaveIcon from '@mui/icons-material/Save';
 
 import TitlePage from '@/components/TitlePage';
 import TitleInput from '@/components/TitleInput';
+import { generalEndpoints } from '@/utilities/endpoints';
+import postUsers from '@/services/postUsers';
 import {
   stylesContainerBox,
   stylesContainerInput,
@@ -11,14 +13,59 @@ import {
 } from '@/components/Habitaciones/stylesHabitaciones';
 
 const FormCreateUsuario = () => {
+  const [datos, setDatos] = useState({
+    username: '',
+    password: '',
+    email: '',
+    confirm: '',
+  });
+  const [rol, setRol] = useState('');
+
+  const identifier = 'test@email.com';
+  const password = 'Test123';
+  const endpoint = generalEndpoints.usuario;
+
+  const handleInputChange = event => {
+    setDatos({ ...datos, [event.target.name]: event.target.value });
+  };
+
+  const sendDatos = async event => {
+    event.preventDefault();
+
+    const confirmed = true;
+    const blocked = true;
+
+    if (datos.password == datos.confirm) {
+      if (datos.username && datos.password && datos.email && datos.confirm && rol) {
+        const dataUser = {
+          username: datos.username,
+          password: datos.password,
+          email: datos.email,
+          confirmed,
+          blocked,
+        };
+        const dataRole = { role: { id: rol } };
+
+        await postUsers(identifier, password, endpoint, dataUser, dataRole);
+        location.reload();
+      } else {
+        alert('Por favor, llene todos los campos');
+      }
+    } else {
+      alert('Las contraseñas no coinciden');
+    }
+  };
+
   return (
-    <Container component='section' sx={[stylesContainerSection, { width: 400, height: 502.25 }]}>
+    <Container component='section' disableGutters sx={[stylesContainerSection, { width: 400, height: 625.25 }]}>
       <CssBaseline />
       <TitlePage titlePage='Registro de Nuevo Usuario' />
       <Box component='form' sx={stylesContainerBox}>
         <Box component='div' sx={stylesContainerInput}>
           <TitleInput titleInput='Nombre de usuario' />
           <TextField
+            onChange={handleInputChange}
+            name='username'
             variant='outlined'
             type='text'
             margin='none'
@@ -30,8 +77,25 @@ const FormCreateUsuario = () => {
           />
         </Box>
         <Box component='div' sx={stylesContainerInput}>
+          <TitleInput titleInput='Correo Electronico' />
+          <TextField
+            onChange={handleInputChange}
+            name='email'
+            variant='outlined'
+            type='text'
+            margin='none'
+            size='small'
+            placeholder='Email'
+            required
+            fullWidth
+            autoFocus
+          />
+        </Box>
+        <Box component='div' sx={stylesContainerInput}>
           <TitleInput titleInput='Contraseña' />
           <TextField
+            onChange={handleInputChange}
+            name='password'
             variant='outlined'
             type='password'
             margin='none'
@@ -44,6 +108,8 @@ const FormCreateUsuario = () => {
         <Box component='div' sx={stylesContainerInput}>
           <TitleInput titleInput='Confirmar contraseña' />
           <TextField
+            onChange={handleInputChange}
+            name='confirm'
             variant='outlined'
             type='password'
             margin='none'
@@ -54,9 +120,21 @@ const FormCreateUsuario = () => {
           />
         </Box>
         <Box component='div' sx={stylesContainerInput}>
-          <TitleInput titleInput='Selecciona rol' />
+          <TitleInput titleInput='Selecciona un rol' />
+          <FormControlLabel
+            control={<Checkbox name='admin' value={'4'} onChange={e => setRol(e.target.value)} />}
+            label='Administrador'
+          />
+          <FormControlLabel
+            control={<Checkbox name='recepcion' value={'3'} onChange={e => setRol(e.target.value)} />}
+            label='Recepcionista'
+          />
+          <FormControlLabel
+            control={<Checkbox name='encargado' value={'5'} onChange={e => setRol(e.target.value)} />}
+            label='Encargado'
+          />
         </Box>
-        <Button variant='contained' size='large' startIcon={<SaveIcon />} sx={{ marginTop: 2 }}>
+        <Button variant='contained' onClick={sendDatos} size='large' startIcon={<SaveIcon />} sx={{ marginTop: 2 }}>
           Registrar Usuario
         </Button>
       </Box>
