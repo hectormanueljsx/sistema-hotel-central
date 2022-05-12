d294b3051d8ced1c3cfd69abe3f44cf3d7dd4b20
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-12 13:14:17 -0500

    Update TableViewHabitacionPendiente.jsx

--- src/components/Reportes/Habitacion/TableViewHabitacionPendiente.jsx
+++ src/components/Reportes/Habitacion/TableViewHabitacionPendiente.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
   Box,
   Container,
@@ -8,83 +8,67 @@ import {
   TableCell,
   TableContainer,
   TableHead,
-  TablePagination,
   TableRow,
   TextField,
 } from '@mui/material';
 
 import TitlePage from '@/components/TitlePage';
-import TitleInput from '@/components/TitleInput';
-import { stylesContainerSection, stylesContainerBox } from '@/components/Reportes/stylesReportes';
+import { stylesContainerSection, stylesTableCell, stylesContainerInput } from '@/components/Reportes/stylesReportes';
 
 const columns = [
-  { id: 'num_registro', label: 'N° Registro', width: 150 },
-  { id: 'llegada', label: 'Llegada', width: 200 },
-  { id: 'salida', label: 'Salida', width: 200 },
+  { id: 'num_registro', label: 'N° de Registro', width: 100 }, //
+  { id: 'llegada', label: 'Llegada', width: 130 },
+  { id: 'salida', label: 'Salida', width: 130 },
   { id: 'cliente', label: 'Cliente', width: 350 },
-  { id: 'total', label: 'Total a pagar', width: 200 },
+  { id: 'total', label: 'Total a Pagar', width: 150 },
   { id: 'pagado', label: 'Pagado', width: 150 },
   { id: 'restante', label: 'Restante', width: 150 },
-  { id: 'estado', label: 'Estado', width: 300 },
+  { id: 'estado', label: 'Estado', width: 192 },
 ];
 
 const TableViewHabitacionPendiente = () => {
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-
-  const identifier = 'test@email.com';
-  const password = 'Test123';
-  const endpoint = 'egresos';
-  const atribute = 'facturado';
-  const parameter = 1;
-  const { list, loading, error } = useGetSpecific(identifier, password, endpoint, atribute, parameter);
-  console.log(list);
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = event => {
-    setRowsPerPage(+event.target.value);
-    setPage(0);
-  };
-
   return (
-    <Container component='section' sx={[stylesContainerSection, { width: 1000 }]}>
+    <Container component='section' disableGutters maxWidth='xl' sx={[stylesContainerSection, { width: 1400 }]}>
       <CssBaseline />
-      <TitlePage titlePage='Registros con saldos pendientes' />
+      <TitlePage titlePage='Histórico de Egresos' />
+      <Box component='div' sx={[stylesContainerInput, { width: 400 }]}>
+        <TextField
+          variant='outlined'
+          type='search'
+          margin='none'
+          size='small'
+          placeholder='Egreso'
+          required
+          fullWidth
+          autoFocus
+        />
+      </Box>
       <Box component='div'>
-        <Box component='form' sx={[stylesContainerBox, { width: 200 }]}>
-         //AQUI VA BUSCAR Y N° DE REGISTROS
-        </Box>
-
         <TableContainer>
           <Table>
             <TableHead>
               <TableRow>
                 {columns.map((column, index) => (
-                  <TableCell key={index} sx={{ width: column.width }}>
+                  <TableCell key={index} sx={[stylesTableCell, { width: column.width }]}>
                     {column.label}
                   </TableCell>
                 ))}
               </TableRow>
             </TableHead>
             <TableBody>
-              {loading && console.log('Cargando')}
-              {error && console.log('Error')}
-              //AQUI SE RENDERIZAN LOS DATOS
+              <TableRow>
+                <TableCell sx={stylesTableCell}>206062</TableCell>
+                <TableCell sx={stylesTableCell}>23-02-2022</TableCell>
+                <TableCell sx={stylesTableCell}>24-02-2022</TableCell>
+                <TableCell sx={stylesTableCell}>RUFINO YTURBE SANTIAGO</TableCell>
+                <TableCell sx={stylesTableCell}>10,220.00</TableCell>
+                <TableCell sx={stylesTableCell}>7,480.00</TableCell>
+                <TableCell sx={stylesTableCell}>1590.00</TableCell>
+                <TableCell sx={stylesTableCell}>CHECK-OUT</TableCell>
+              </TableRow>
             </TableBody>
           </Table>
         </TableContainer>
-        <TablePagination
-          rowsPerPageOptions={[]}
-          component='div'
-          count={list.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onPageChange={handleChangePage}
-          onRowsPerPageChange={handleChangeRowsPerPage}
-        />
       </Box>
     </Container>
   );
