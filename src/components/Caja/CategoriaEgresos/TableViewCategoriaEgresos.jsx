b54ea16445ee974e35aafebfd6649846609a7d3f
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-17 18:47:41 -0500

    Update TableViewCategoriaEgresos.jsx

--- src/components/Caja/CategoriaEgresos/TableViewCategoriaEgresos.jsx
+++ src/components/Caja/CategoriaEgresos/TableViewCategoriaEgresos.jsx
@@ -19,7 +19,7 @@ import Loader from '@/components/Loader';
 import useGetGeneralTable from '@/hooks/useGetGeneralTable';
 import deleteGeneralTable from '@/services/deleteGeneralTable';
 import { generalEndpoints } from '@/utilities/endpoints';
-import { stylesContainerSection } from '@/components/Caja/stylesCaja';
+import { stylesAccordion, stylesContainerSection } from '@/components/Caja/stylesCaja';
 
 const TableViewCategoriaEgresos = () => {
   const [expanded, setExpanded] = useState(false);
@@ -31,19 +31,21 @@ const TableViewCategoriaEgresos = () => {
 
   const handleChange = panel => (event, isExpanded) => setExpanded(isExpanded ? panel : false);
 
-  const { list, loading, error } = useGetGeneralTable(identifier, password, endpointCategoria);
+  const deleteCategoria = async (id, subcategoria) => {
+    if (subcategoria === 0) {
+      await deleteGeneralTable(identifier, password, endpointCategoria, id);
+      location.reload();
+    } else {
+      alert('No se puede eliminar una categoria con subcategorias');
+    }
+  };
 
   const deleteSubcategoria = async id => {
     await deleteGeneralTable(identifier, password, endpointSubategoria, id);
     location.reload();
   };
 
-  const deleteCategoria = async (id, subcategoria) => {
-    subcategoria == 0
-      ? await deleteGeneralTable(identifier, password, endpointCategoria, id)
-      : alert('No se puede eliminar una categoria con subcategorias');
-    location.reload();
-  };
+  const { list, loading, error } = useGetGeneralTable(identifier, password, endpointCategoria);
 
   return (
     <Container component='section' disableGutters sx={[stylesContainerSection, { width: 1000 }]}>
@@ -57,28 +59,40 @@ const TableViewCategoriaEgresos = () => {
           return (
             <Accordion key={index} expanded={expanded === `panel${index}`} onChange={handleChange(`panel${index}`)}>
               <AccordionSummary key={index} expandIcon={<ExpandMore />}>
-                <Typography key={index} sx={{ fontWeight: '700' }}>
-                  {categoria}
-                </Typography>
-                <IconButton color='error' size='small' onClick={() => deleteCategoria(id, subcategorias.length)}>
-                  <DeleteIcon />
-                </IconButton>
-              </AccordionSummary>
-              {subcategorias.map((value, idx) => {
-                return (
-                  <AccordionDetails
-                    key={idx}
-                    sx={{ display: 'flex', alignItems: 'center', padding: 0, marginBottom: 2 }}
-                  >
-                    <ListItemIcon>
-                      <ArrowRightIcon sx={{ marginLeft: 5 }} />
-                    </ListItemIcon>
-                    <Typography key={idx} sx={{ fontWeight: '300' }}>
-                      {value.descripcion}
-                    </Typography>
-                    <IconButton color='error' size='small' onClick={() => deleteSubcategoria(value.id)}>
+                <Box sx={[stylesAccordion, { width: 850 }]}>
+                  <Typography key={index} sx={{ fontWeight: '700' }}>
+                    {categoria}
+                  </Typography>
+                  {subcategorias.length === 0 && (
+                    <IconButton
+                      color='error'
+                      size='small'
+                      sx={{}}
+                      onClick={() => deleteCategoria(id, subcategorias.length)}
+                    >
                       <DeleteIcon />
                     </IconButton>
+                  )}
+                </Box>
+              </AccordionSummary>
+              {subcategorias.map((subitem, idx) => {
+                const { descripcion, id } = subitem;
+
+                return (
+                  <AccordionDetails key={idx} sx={{ padding: 0, marginBottom: 1 }}>
+                    <Box sx={[stylesAccordion, { width: 866 }]}>
+                      <Box sx={[stylesAccordion, { marginLeft: 5 }]}>
+                        <ListItemIcon sx={{ minWidth: 0 }}>
+                          <ArrowRightIcon />
+                        </ListItemIcon>
+                        <Typography key={idx} sx={{ fontWeight: '300' }}>
+                          {descripcion}
+                        </Typography>
+                      </Box>
+                      <IconButton color='error' size='small' onClick={() => deleteSubcategoria(id)}>
+                        <DeleteIcon />
+                      </IconButton>
+                    </Box>
                   </AccordionDetails>
                 );
               })}
