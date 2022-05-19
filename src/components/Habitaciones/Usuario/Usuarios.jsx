12903c397b60c1ff40030ed02fd5f75770ce4c5a
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-19 12:14:57 -0500

    Add validations for messages errors or success

--- src/components/Habitaciones/Usuario/Usuarios.jsx
+++ src/components/Habitaciones/Usuario/Usuarios.jsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Container, CssBaseline } from '@mui/material';
 
+import AlertGlobalForms from '@/components/AlertGlobalForms';
 import FormCreateUsuario from '@/components/Habitaciones/Usuario/FormCreateUsuario';
 import TableViewUsuarios from '@/components/Habitaciones/Usuario/TableViewUsuarios';
 
 const Usuarios = () => {
+  const [messageError, setMessageError] = useState('');
+  const [messageSuccess, setMessageSuccess] = useState('');
+  const [openAlert, setOpenAlert] = useState(true);
+
   return (
     <Container component='section' disableGutters maxWidth='xl'>
       <CssBaseline />
+      {messageError && (
+        <AlertGlobalForms message={messageError} open={openAlert} setOpen={setOpenAlert} severity='error' />
+      )}
+      {messageSuccess && (
+        <AlertGlobalForms message={messageSuccess} open={openAlert} setOpen={setOpenAlert} severity='success' />
+      )}
       <Box sx={{ display: 'flex' }}>
-        <FormCreateUsuario />
+        <FormCreateUsuario
+          setMessageError={setMessageError}
+          setMessageSuccess={setMessageSuccess}
+          setOpenAlert={setOpenAlert}
+        />
         <TableViewUsuarios />
       </Box>
     </Container>
