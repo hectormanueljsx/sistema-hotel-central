b7ea59d620465ef833182dd06ec0f4a846b75849
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-19 01:41:33 -0500

    Update CategoriaEgresos.jsx

--- src/components/Caja/CategoriaEgresos/CategoriaEgresos.jsx
+++ src/components/Caja/CategoriaEgresos/CategoriaEgresos.jsx
@@ -4,22 +4,28 @@ import { Box, Container, CssBaseline } from '@mui/material';
 import AlertGlobalForms from '@/components/AlertGlobalForms';
 import FormCreateCategoriaEgresos from '@/components/Caja/CategoriaEgresos/FormCreateCategoriaEgresos';
 import TableViewCategoriaEgresos from '@/components/Caja/CategoriaEgresos/TableViewCategoriaEgresos';
-import { stylesBoxAlerts } from '@/components/Caja/stylesCaja';
 
 const CategoriaEgresos = () => {
   const [messageError, setMessageError] = useState('');
   const [messageSuccess, setMessageSuccess] = useState('');
+  const [openAlert, setOpenAlert] = useState(false);
 
   return (
     <Container component='section' disableGutters maxWidth='xl'>
       <CssBaseline />
       <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <Box sx={stylesBoxAlerts}>
-          {messageError && <AlertGlobalForms message={messageError} severity='error' />}
-          {messageSuccess && <AlertGlobalForms message={messageSuccess} severity='success' />}
-        </Box>
+        {messageError && (
+          <AlertGlobalForms message={messageError} open={openAlert} setOpen={setOpenAlert} severity='error' />
+        )}
+        {messageSuccess && (
+          <AlertGlobalForms message={messageSuccess} open={openAlert} setOpen={setOpenAlert} severity='success' />
+        )}
         <Box sx={{ display: 'flex' }}>
-          <FormCreateCategoriaEgresos setMessageError={setMessageError} setMessageSuccess={setMessageSuccess} />
+          <FormCreateCategoriaEgresos
+            setMessageError={setMessageError}
+            setMessageSuccess={setMessageSuccess}
+            setOpenAlert={setOpenAlert}
+          />
           <TableViewCategoriaEgresos />
         </Box>
       </Box>
