3232e1bf0af5615bf7593a638bc13238af48b55a
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-18 20:51:44 -0500

    Update CategoriaEgresos.jsx

--- src/components/Caja/CategoriaEgresos/CategoriaEgresos.jsx
+++ src/components/Caja/CategoriaEgresos/CategoriaEgresos.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Container, CssBaseline } from '@mui/material';
 
+import AlertGlobalForms from '@/components/AlertGlobalForms';
 import FormCreateCategoriaEgresos from '@/components/Caja/CategoriaEgresos/FormCreateCategoriaEgresos';
 import TableViewCategoriaEgresos from '@/components/Caja/CategoriaEgresos/TableViewCategoriaEgresos';
+import { stylesBoxAlerts } from '@/components/Caja/stylesCaja';
 
 const CategoriaEgresos = () => {
+  const [messageError, setMessageError] = useState('');
+  const [messageSuccess, setMessageSuccess] = useState('');
+
   return (
     <Container component='section' disableGutters maxWidth='xl'>
       <CssBaseline />
-      <Box sx={{ display: 'flex' }}>
-        <FormCreateCategoriaEgresos />
-        <TableViewCategoriaEgresos />
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <Box sx={stylesBoxAlerts}>
+          {messageError && <AlertGlobalForms messageError={messageError} severity='error' />}
+          {messageSuccess && <AlertGlobalForms messageError={messageSuccess} severity='success' />}
+        </Box>
+        <Box sx={{ display: 'flex' }}>
+          <FormCreateCategoriaEgresos setMessageError={setMessageError} setMessageSuccess={setMessageSuccess} />
+          <TableViewCategoriaEgresos />
+        </Box>
       </Box>
     </Container>
   );
