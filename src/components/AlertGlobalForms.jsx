2f3e9d2fe4a62946b00c52e2604a098fad2bd0e3
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-19 22:03:12 -0500

    Update AlertGlobalForms.jsx

--- src/components/AlertGlobalForms.jsx
+++ src/components/AlertGlobalForms.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import { Alert } from '@mui/material';
+import React, { forwardRef } from 'react';
+import { Snackbar } from '@mui/material';
+import MuiAlert from '@mui/material/Alert';
+import Slide from '@mui/material/Slide';
 
-const AlertGlobalForms = ({ message, severity }) => {
+const AlertGlobalForms = ({ message, severity, open, setOpen }) => {
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') return;
+
+    open ? setOpen(false) : setOpen(true);
+  };
+
+  const Alert = forwardRef(function Alert(props, ref) {
+    return (
+      <MuiAlert sx={{ marginTop: 7 }} ref={ref} variant='filled' severity={severity}>
+        {message}
+      </MuiAlert>
+    );
+  });
+
   return (
-    <Alert variant='outlined' severity={severity}>
-      {message}
-    </Alert>
+    <Snackbar
+      open={open}
+      autoHideDuration={1300}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      TransitionComponent={Slide}
+    >
+      <Alert />
+    </Snackbar>
   );
 };
 
