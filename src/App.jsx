694aad8d0194dd9776633a8e7eaf031c26669c6d
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-21 16:39:14 -0500

    Add routing with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// Sección de Caja
+import CategoriaEgresos from '@/components/Caja/CategoriaEgresos/CategoriaEgresos';
+// Sección de Habitaciones
+import Tarifas from '@/components/Habitaciones/Tarifa/Tarifas';
+import Usuarios from '@/components/Habitaciones/Usuario/Usuarios';
+
+import Navbar from '@/components/Navbar/Navbar';
+import NotFound from '@/components/NotFound/NotFound';
 
 const theme = createTheme({
   palette: {
@@ -10,10 +20,20 @@ const theme = createTheme({
 });
 
 const App = () => {
-  const identifier = 'test@email.com';
-  const password = 'Test123';
-
-  return <ThemeProvider theme={theme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/caja/categoria-egresos' element={<CategoriaEgresos />} />
+          <Route path='/habitaciones/tarifas' element={<Tarifas />} />
+          <Route path='/habitaciones/habitaciones' />
+          <Route path='/habitaciones/usuarios' element={<Usuarios />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 };
 
 export default App;
