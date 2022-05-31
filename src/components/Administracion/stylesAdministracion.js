cd34f585735e18eb46491fa78bad703622ec1fc9
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-05-31 11:49:38 -0500

    Create stylesAdministracion.js

---
+++ src/components/Administracion/stylesAdministracion.js
@@ -0,0 +1,31 @@
+const stylesContainerSection = {
+  backgroundColor: '#ffffff',
+  borderRadius: 1,
+  boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.25)',
+  padding: 3,
+  marginTop: 6,
+};
+
+const stylesContainerBox = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+};
+
+const stylesContainerInput = {
+  width: '100%',
+  marginBottom: 2,
+};
+
+const stylesTableCell = {
+  padding: 1,
+};
+
+const stylesModal = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+};
+
+export { stylesContainerSection, stylesContainerBox, stylesContainerInput, stylesTableCell, stylesModal };
