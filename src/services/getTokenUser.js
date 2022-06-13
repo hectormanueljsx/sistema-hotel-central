799494b202c0cb49c8285a4e753633fb3f683499
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-06-13 10:01:32 -0500

    Update getTokenUser.js

--- src/services/getTokenUser.js
+++ src/services/getTokenUser.js
@@ -2,9 +2,9 @@ import apiConfig from '@/api/apiConfig';
 
 const getTokenUser = async (identifier, password) => {
   try {
-    const { data } = await apiConfig.post('auth/local', { identifier, password });
+    const { data: jwt } = await apiConfig.post('auth/local', { identifier, password });
 
-    return data.jwt;
+    return jwt;
   } catch (error) {
     return error;
   }
