9d8a8d5ae100216ffb1cdde0c028a00ab8be698a
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-06-13 16:55:37 -0500

    Create NotFound

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
-import { CssBaseline } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
 
-import '@/components/NotFound/NotFound.css';
+import {
+  stylesErrorNumber,
+  stylesErrorText,
+  stylesSectionContainer,
+  stylesSectionText,
+} from '@/components/NotFound/NotFoundStyles';
 
 const NotFound = () => {
   return (
-    <section className='section-container'>
-      <CssBaseline />
-      <section className='section-text'>
-        <h1 className='error-number'>404</h1>
-        <h2 className='error-text'>Página no encontrada</h2>
-      </section>
-    </section>
+    <Container component='section' disableGutters maxWidth='xl' sx={stylesSectionContainer}>
+      <Box sx={stylesSectionText}>
+        <Typography component='h1' sx={stylesErrorNumber}>
+          404
+        </Typography>
+        <Typography component='h2' sx={stylesErrorText}>
+          Página no encontrada
+        </Typography>
+      </Box>
+    </Container>
   );
 };
 
---
+++ src/components/NotFound/NotFoundStyles.js
@@ -0,0 +1,30 @@
+import { css } from '@emotion/react';
+
+export const stylesSectionContainer = css`
+  display: flex;
+  width: 100%;
+  height: calc(100vh - 80px);
+`;
+
+export const stylesSectionText = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+export const stylesErrorNumber = css`
+  font-size: 120px;
+  font-weight: 700;
+  color: #51bab3;
+  margin: 0;
+`;
+
+export const stylesErrorText = css`
+  font-size: 60px;
+  font-weight: 500;
+  color: #000000;
+  margin: 0;
+`;
