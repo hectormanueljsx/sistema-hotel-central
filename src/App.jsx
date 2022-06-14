b7ab83693f888a554c852bc85475dbb62f1f1087
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-06-13 22:40:42 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Tarifas from '@/components/Habitaciones/Tarifa/Tarifas';
 import Habitaciones from '@/components/Habitaciones/Habitaciones/Habitaciones';
 import Usuarios from '@/components/Administracion/Usuario/Usuarios';
 import Navbar from '@/components/Navbar/Navbar';
+import ProtectedRoute from '@/components/ProtectedRoute/ProtectedRoute';
 import Login from '@/components/Login/Login';
 import NotFound from '@/components/NotFound/NotFound';
 
@@ -26,14 +27,63 @@ const App = () => {
       <CssBaseline />
       <Navbar />
       <Routes>
-        <Route path='/' />
+        <Route
+          path='/'
+          element={
+            <ProtectedRoute>
+              <CategoriaEgresos />
+            </ProtectedRoute>
+          }
+        />
         <Route path='/login' element={<Login />} />
-        <Route path='/caja/categorias-de-egresos' element={<CategoriaEgresos />} />
-        <Route path='/caja/egresos' element={<Egresos />} />
-        <Route path='/reportes/mantenimiento' element={<Mantenimiento />} />
-        <Route path='/habitaciones/tarifas' element={<Tarifas />} />
-        <Route path='/habitaciones/habitaciones' element={<Habitaciones />} />
-        <Route path='/administracion/usuarios' element={<Usuarios />} />
+        <Route
+          path='/caja/categorias-de-egresos'
+          element={
+            <ProtectedRoute>
+              <CategoriaEgresos />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/caja/egresos'
+          element={
+            <ProtectedRoute>
+              <Egresos />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/reportes/mantenimiento'
+          element={
+            <ProtectedRoute>
+              <Mantenimiento />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/habitaciones/tarifas'
+          element={
+            <ProtectedRoute>
+              <Tarifas />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/habitaciones/habitaciones'
+          element={
+            <ProtectedRoute>
+              <Habitaciones />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path='/administracion/usuarios'
+          element={
+            <ProtectedRoute>
+              <Usuarios />
+            </ProtectedRoute>
+          }
+        />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </ThemeProvider>
