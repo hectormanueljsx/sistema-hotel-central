8528c9e7e7c715f22e70362305e1a7dfab36482b
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-06-13 22:40:33 -0500

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { CssBaseline } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import Dropdown from '@/components/Navbar/Dropdown';
 import {
@@ -27,92 +27,104 @@ const Navbar = () => {
     <section className='navbar-fluid'>
       <CssBaseline />
       <nav className='navbar'>
-        <NavLink to='/' className='navbar-link'>
+        <Link to='/' className='navbar-link'>
           <img className='logotipo-hc' src={Logotipo} alt='Logotipo Hotel Central' />
-        </NavLink>
+        </Link>
 
-        <ul className='navbar-items'>
-          {navItems.map(item => {
-            if (item.title === 'Reservas') {
-              return (
-                <li
-                  key={item.id}
-                  className={item.cName}
-                  onMouseEnter={() => setDropdownReservas(true)}
-                  onMouseLeave={() => setDropdownReservas(false)}
-                >
-                  <button>{item.title}</button>
-                </li>
-              );
-            }
+        {user ? (
+          <>
+            <ul className='navbar-items'>
+              {navItems.map(item => {
+                if (item.title === 'Reservas') {
+                  return (
+                    <li
+                      key={item.id}
+                      className={item.cName}
+                      onMouseEnter={() => setDropdownReservas(true)}
+                      onMouseLeave={() => setDropdownReservas(false)}
+                    >
+                      <button>{item.title}</button>
+                    </li>
+                  );
+                }
 
-            if (item.title === 'Cotizar') {
-              return (
-                <li
-                  key={item.id}
-                  className={item.cName}
-                  onMouseEnter={() => setDropdownCotizar(true)}
-                  onMouseLeave={() => setDropdownCotizar(false)}
-                >
-                  <button>{item.title}</button>
-                </li>
-              );
-            }
+                if (item.title === 'Cotizar') {
+                  return (
+                    <li
+                      key={item.id}
+                      className={item.cName}
+                      onMouseEnter={() => setDropdownCotizar(true)}
+                      onMouseLeave={() => setDropdownCotizar(false)}
+                    >
+                      <button>{item.title}</button>
+                    </li>
+                  );
+                }
 
-            if (item.title === 'Caja') {
-              return (
-                <li
-                  key={item.id}
-                  className={item.cName}
-                  onMouseEnter={() => setDropdownCaja(true)}
-                  onMouseLeave={() => setDropdownCaja(false)}
-                >
-                  <button>{item.title}</button>
-                  {dropdownCaja && <Dropdown dropdownItem={cajaDropdown} />}
-                </li>
-              );
-            }
+                if (item.title === 'Caja') {
+                  return (
+                    <li
+                      key={item.id}
+                      className={item.cName}
+                      onMouseEnter={() => setDropdownCaja(true)}
+                      onMouseLeave={() => setDropdownCaja(false)}
+                    >
+                      <button>{item.title}</button>
+                      {dropdownCaja && <Dropdown dropdownItem={cajaDropdown} />}
+                    </li>
+                  );
+                }
 
-            if (item.title === 'Reportes') {
-              return (
-                <li
-                  key={item.id}
-                  className={item.cName}
-                  onMouseEnter={() => setDropdownReportes(true)}
-                  onMouseLeave={() => setDropdownReportes(false)}
-                >
-                  <button>{item.title}</button>
-                  {dropdownReportes && <Dropdown dropdownItem={reportesDropdown} />}
-                </li>
-              );
-            }
+                if (item.title === 'Reportes') {
+                  return (
+                    <li
+                      key={item.id}
+                      className={item.cName}
+                      onMouseEnter={() => setDropdownReportes(true)}
+                      onMouseLeave={() => setDropdownReportes(false)}
+                    >
+                      <button>{item.title}</button>
+                      {dropdownReportes && <Dropdown dropdownItem={reportesDropdown} />}
+                    </li>
+                  );
+                }
 
-            if (item.title === 'Habitaciones') {
-              return (
-                <li
-                  key={item.id}
-                  className={item.cName}
-                  onMouseEnter={() => setDropdownHabitaciones(true)}
-                  onMouseLeave={() => setDropdownHabitaciones(false)}
-                >
-                  <button>{item.title}</button>
-                  {dropdownHabitaciones && <Dropdown dropdownItem={habitacionesDropdown} />}
-                </li>
-              );
-            }
-          })}
-        </ul>
-        <ul className='navbar-items'>
-          <li
-            key={0}
-            className='navbar-item'
-            onMouseEnter={() => setDropdownUser(true)}
-            onMouseLeave={() => setDropdownUser(false)}
-          >
-            <button>{user}</button>
-            {dropdownUser && <Dropdown dropdownItem={userDropdown} />}
-          </li>
-        </ul>
+                if (item.title === 'Habitaciones') {
+                  return (
+                    <li
+                      key={item.id}
+                      className={item.cName}
+                      onMouseEnter={() => setDropdownHabitaciones(true)}
+                      onMouseLeave={() => setDropdownHabitaciones(false)}
+                    >
+                      <button>{item.title}</button>
+                      {dropdownHabitaciones && <Dropdown dropdownItem={habitacionesDropdown} />}
+                    </li>
+                  );
+                }
+              })}
+            </ul>
+            <ul className='navbar-items'>
+              <li
+                key={0}
+                className='navbar-item'
+                onMouseEnter={() => setDropdownUser(true)}
+                onMouseLeave={() => setDropdownUser(false)}
+              >
+                <button>{user}</button>
+                {dropdownUser && <Dropdown dropdownItem={userDropdown} />}
+              </li>
+            </ul>
+          </>
+        ) : (
+          <ul className='navbar-items'>
+            <li key='login' className='navbar-item'>
+              <button style={{ padding: 0 }}>
+                <Link to='/login'>Iniciar Sesión</Link>
+              </button>
+            </li>
+          </ul>
+        )}
       </nav>
     </section>
   );
