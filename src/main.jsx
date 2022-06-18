3ffc19d33ceb2bfb831a4c1fb4bbe359d40d9d4f
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-06-17 21:24:00 -0500

    Add and create class css of z-index to alert

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from '@/App';
+import 'sweetalert2/dist/sweetalert2.min.css';
+import '@/main.css';
 
 ReactDOM.render(
   <React.StrictMode>
