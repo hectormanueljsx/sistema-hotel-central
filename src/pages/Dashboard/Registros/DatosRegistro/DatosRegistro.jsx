fce7f80bebd31899b13f401ddf02680136106fd0
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-07-17 16:16:03 -0500

    Update DatosRegistro.jsx

--- src/pages/Dashboard/Registros/DatosRegistro/DatosRegistro.jsx
+++ src/pages/Dashboard/Registros/DatosRegistro/DatosRegistro.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Box } from '@mui/material';
 
-import { stylesWrapperGeneral } from '@/pages/Dashboard/Registros/DatosRegistro/DatosRegistroStyles';
 import FormDatosRegistro from '@/pages/Dashboard/Registros/DatosRegistro/FormDatosRegistro';
+import { stylesWrapperGeneral } from '@/pages/Dashboard/Registros/DatosRegistro/DatosRegistroStyles';
 
 const DatosRegistro = () => {
   return (
