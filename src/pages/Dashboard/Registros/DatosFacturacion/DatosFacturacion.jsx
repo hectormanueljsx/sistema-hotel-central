07e743a7f15e394810b04b92188d80ff106828ed
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-07-18 12:12:32 -0500

    Create module DatosFacturacion

---
+++ src/pages/Dashboard/Registros/DatosFacturacion/DatosFacturacion.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+import { stylesWrapperGeneral } from '@/pages/Dashboard/Registros/DatosFacturacion/DatosFacturacionStyles';
+
+const DatosFacturacion = () => {
+  return (
+    <Box component='section' sx={stylesWrapperGeneral}>
+      DatosFacturacion
+    </Box>
+  );
+};
+
+export default DatosFacturacion;
---
+++ src/pages/Dashboard/Registros/DatosFacturacion/DatosFacturacionStyles.js
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+export const stylesWrapperGeneral = css`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 32px;
+  width: 1432px;
+  height: max-content;
+  margin: 32px auto;
+`;
