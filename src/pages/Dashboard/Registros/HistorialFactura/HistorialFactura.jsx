f4e3655742b5cdaf65d8bc9f54aa207e9ad22e56
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-07-18 12:12:49 -0500

    Create module HistorialFactura

---
+++ src/pages/Dashboard/Registros/HistorialFactura/HistorialFactura.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+import { stylesWrapperGeneral } from '@/pages/Dashboard/Registros/HistorialFactura/HistorialFacturaStyles';
+
+const HistorialFactura = () => {
+  return (
+    <Box component='section' sx={stylesWrapperGeneral}>
+      HistorialFactura
+    </Box>
+  );
+};
+
+export default HistorialFactura;
