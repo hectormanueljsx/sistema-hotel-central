d873e227c0eebd88b87bb05096d2ec4a4016b256
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-07-18 12:13:04 -0500

    Create module HistorialPago

---
+++ src/pages/Dashboard/Registros/HistorialPago/HistorialPago.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+import { stylesWrapperGeneral } from '@/pages/Dashboard/Registros/HistorialPago/HistorialPagoStyles';
+
+const HistorialPago = () => {
+  return (
+    <Box component='section' sx={stylesWrapperGeneral}>
+      HistorialPago
+    </Box>
+  );
+};
+
+export default HistorialPago;
