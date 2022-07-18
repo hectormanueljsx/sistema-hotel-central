ba034c66db9d0fd571df58454416fbaa308ee9af
Hector Manuel Santos Bautista <hectormanueljsx@gmail.com>
2022-07-18 12:12:13 -0500

    Create module CargosExtras

---
+++ src/pages/Dashboard/Registros/CargosExtras/CargosExtras.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+import { stylesWrapperGeneral } from '@/pages/Dashboard/Registros/CargosExtras/CargosExtrasStyles';
+
+const CargosExtras = () => {
+  return (
+    <Box component='section' sx={stylesWrapperGeneral}>
+      CargosExtras
+    </Box>
+  );
+};
+
+export default CargosExtras;
